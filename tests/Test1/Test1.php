1f2cd01050fec2c2bf87d52f7f9bf54ff0ba8024
Stefan Kamsker <stefan.kamsker@virtual-identity.com>
2018-12-18 18:20:35 +0100

    update

---
+++ tests/Test1/Test1.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests\Test1;
+
+use PHPUnit\Framework\TestCase;
+
+class Test1 extends TestCase
+{
+
+    public function test1()
+    {
+        $this->assertEquals(1, 1);
+    }
+
+    public function test2()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
